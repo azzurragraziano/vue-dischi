9386a51353c95727f8fd87f4c6321263d0c95330
azzurragraziano <azzurragraziano02@email.com>
2022-07-08 16:16:38 +0200

    header

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,37 @@
 <template>
   <div id="app">
     <HeaderComponent />
+
+    <main>
+      <MainComponent />
+    </main>
   </div>
 </template>
 
 <script>
 import HeaderComponent from './components/HeaderComponent.vue'
+import MainComponent from './components/MainComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HeaderComponent
+    HeaderComponent,
+    MainComponent
   }
 }
 </script>
 
 <style lang="scss">
-@import '@/assets/style/common.scss';
+@import '@/assets/style/variables.scss';
 
+// reset
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+#app {
+  background-color: $primary_color;
+}
 </style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <header>
+    <div class="logo">
+        <img src="@/assets/img/logo.png" alt="">
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+    name: "HeaderComponent"
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/style/variables.scss';
+
+    header {
+        background-color: $secondary_color;
+
+        img {
+            width: 50px;
+            margin: 0.5rem 0.8rem;
+        }
+    }
+
+    
+</style>
